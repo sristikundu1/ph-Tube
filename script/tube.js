aa8673ef7b5a5312301eac387e02d9407acc7254
sristikundu1 <nidhukundu1234@gmail.com>
2023-09-03 14:27:28 +0600

    sort by button is developed

--- script/tube.js
+++ script/tube.js
@@ -1,3 +1,6 @@
+
+let fetchData;
+let noFetchData;
 const handleCategory = async () => {
     const response = await fetch(
         "https://openapi.programming-hero.com/api/videos/categories"
@@ -17,18 +20,27 @@ const handleCategory = async () => {
 
 };
 
+
+
 const handleLoadData = async (categoryId) => {
     const response = await fetch(
         `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
     );
     const data = await response.json();
 
+    fetchData = data.data;
+    noFetchData = data;
+    console.log(fetchData);
+    
+    displayData();
+};
+function displayData(){
     const noCardContainer = document.getElementById('no-card-container');
     noCardContainer.innerHTML = "";
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = "";
     
-    if (data.status == false) {
+    if (noFetchData.status == false) {
         const div = document.createElement('div');
         div.innerHTML = `
         <img class="mb-8 pl-24 w-48" src="Icon.png" alt="">
@@ -38,7 +50,9 @@ const handleLoadData = async (categoryId) => {
         noCardContainer.appendChild(div);
     }
     else {
-        data.data.forEach(topic => {
+        fetchData.forEach(topic => {
+            //console.log(topic);
+
 
             const div = document.createElement('div');
             // console.log(parseFloat(topic.others.views));
@@ -81,104 +95,36 @@ const handleLoadData = async (categoryId) => {
 
     }
 
+}
+
+function handleSortData(){
     document.getElementById('sortButton').addEventListener('click', function () {
-
-
-        const noCardContainer = document.getElementById('no-card-container');
-        noCardContainer.innerHTML = "";
-        const cardContainer = document.getElementById('card-container');
-        cardContainer.innerHTML = "";
+        comArray = [];
+      
+        let sortValue = sort();
+        console.log(sortValue);
+        fetchData = sortValue;
+        displayData();
         
-        if (data.status == false) {
-            const div = document.createElement('div');
-            div.innerHTML = `
-                <img class="mb-8 pl-24 w-48" src="Icon.png" alt="">
-                <h2 class="text-3xl font-bold">Oops!! Sorry, There is no <br>
-                    content here</h2>
-                `;
-            noCardContainer.appendChild(div);
-        }
-        else {
-
-            let view = [];
-            for (let i = 0; i < data.data.length; i++) {
-                view.push(parseFloat(data.data[i].others.views));
-                //console.log(parseFloat(data.data[i].others.views));
-            }
-            view.sort(function (a, b) { return b - a });
-            
-            for (let j = 0; j < view.length; j++) {
-              
-                data.data.forEach(topic => {
-                   
-
-
-                    if (view[j] === parseFloat(topic.others.views)) {
-
-                        //const id = (topic.category_id);
-                        //console.log(id);
-
-                        const div = document.createElement('div');
-
-                        div.innerHTML = `
-                    <div class="card card-compact w-72 bg-base-100 ">
-                    <figure><img class="relative object-cover w-full h-52 " src=${topic.thumbnail}/></figure>
-                    
-                    ${topic.others.posted_date && `<button id="timeBtn" class=" btn normal-case bg-slate-950  border-slate-950 text-white absolute bottom-44 right-4">${topic.others.posted_date ? convertTime(topic.others.posted_date) : ''}</button>`}
-                     
-                    <div class="card-body">
-                        <div class="flex gap-2 items-center">
-                            <div class="avatar">
-                                <div class="w-14 rounded-full">
-                                <img src=${topic.authors[0].profile_picture} />
-                                </div>
-                            </div>
-                            <div>
-                                <h2 class="card-title">${topic.title}</h2>
-                            </div>
-                        </div>
-            
-                        <div class="flex gap-3">
-                            <div class="ml-16 text-base text-zinc-500">
-                                <p>${topic.authors[0].profile_name}</p>
-                            </div>
-            
-                           
-                            <img class="w-6" src="${topic.authors[0].verified ? `checklist_10629607.png` : ''}"/>
-                           
-            
-                        </div >
-                        <p class="ml-16 text-base text-zinc-500">${topic.others.views} <span>views</span></p>
-                    </div >
-                </div >
-    
-         `;
-                        cardContainer.appendChild(div);
-
-
-                    }
-
-                });
-                
-
-
-            }
-           
-        }
-
-
-
-
-
-
-
+       
+       
+        
+       
     });
-
 
 };
 
+function sort(){
+    let view = []; 
+    const sortedData = fetchData.sort(function (a, b) { return  parseFloat(b.others.views) -  parseFloat(a.others.views) });
+return sortedData;
+  
+}
+
+
 handleCategory();
 handleLoadData("1000");
+handleSortData();
 
 function convertTime(second) {
     const seconds = parseInt(second);
